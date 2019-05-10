b36066db56c42fceb1eeb697b5dc3a088a0ea71b
OsbaldoAguilarContreras <osbaldoaguilar76@gmail.com>
2019-05-10 00:25:33 +0000

    3rd Commit

---
+++ EvenOdd.js
@@ -0,0 +1,29 @@
+var number =Math.floor(Math.random() * 100);
+console.log(number);
+//console.log("The number being checked is " + number);
+
+// Print if the number is odd or even
+if (number%2 == 0){
+     console.log('EVEN');
+    }
+     else
+     {
+         console.log('ODD');
+     }
+     
+     
+     
+     
+     
+     
+
+     //   {return true;
+       /* if (number === true)
+        {
+            console.log(number +"is EVEN");
+        }
+        }
+       return false;
+         if (number === false)
+            console.log(number +"is ODD");
+       */
